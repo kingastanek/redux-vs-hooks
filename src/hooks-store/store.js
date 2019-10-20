2530b0fedac3ce2e5f4c030b6c44402bcc1bfc9b
kingastanek <kin.stanek@gmail.com>
2019-10-20 18:33:16 +0200

    Finishing the Store Hook

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
   const setState = useState(globalState)[1];
 
-  listeners.push(setState);
+  const dispatch = actionIdentifier => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.filter(li => li !== setState);
+    };
+  }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
 };
